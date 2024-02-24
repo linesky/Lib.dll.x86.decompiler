ea0946d36157030b92f8c0339c450bcfac5b2bee
sky line <32512284+linesky@users.noreply.github.com>
2024-02-24 21:21:53 +0000

    Add files via upload

---
+++ builderboard.py
@@ -0,0 +1,15 @@
+xx="""keyss[$1]=tk.Button(self.bottomframe, text='$0',command=ckeyss$1 ,borderwidth=1,bg='red').grid(row=$2, column=$3)
+"""
+print("\x1bc\x1b[41;37m")
+s=input("past key strings separete by , order? ")
+s=s.split(",")
+i=int(input("number of coluns ?"))
+n:int=0
+m:int=0
+for nn in range(len(s)//i):
+    for mm in range(i):
+        xxx=xx.replace("$0",s[nn*i+mm])
+        xxx=xxx.replace("$1",str(nn*i+mm))
+        xxx=xxx.replace("$2",str(nn))
+        xxx=xxx.replace("$3",str(mm))
+        print(xxx)
